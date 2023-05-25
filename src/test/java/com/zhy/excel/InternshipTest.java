b7c916de72063679526e69ad264c6e1d44eb44fc
zhouhongyin <1429855087@qq.com>
2023-05-25 14:03:28 +0800

    专属页面相关

--- src/test/java/com/zhy/excel/InternshipTest.java
+++ src/test/java/com/zhy/excel/InternshipTest.java
@@ -30,15 +30,20 @@ class InternshipTest {
     @Test
     void excel() throws SQLException {
 
-        List<InternshipExcel> internshipExcels = db();
+        List<InternshipExcel> internshipExcels = dbCompany();
 
         Map<String, InternshipExcel> libIntern = new HashMap<>();
 
         internshipExcels.forEach(intern -> {
 
+
             String id = String.valueOf(intern.getId()).trim();
             String name = String.valueOf(intern.getName()).trim();
-            InternshipExcel exist = libIntern.get(name);
+            String companyName = String.valueOf(intern.getCompanyName()).trim();
+
+            String key = name + companyName;
+
+            InternshipExcel exist = libIntern.get(key);
             if (exist != null) {
                 exist.incrDuplicates();
                 return;
@@ -47,20 +52,22 @@ class InternshipTest {
             InternshipExcel internshipExcel = new InternshipExcel();
             internshipExcel.setId(id);
             internshipExcel.setName(name);
+            internshipExcel.setCompanyName(companyName);
             internshipExcel.incrDuplicates();
 
-            libIntern.put(name, internshipExcel);
+            libIntern.put(key, internshipExcel);
 
         });
 
-        ExcelReader reader = ExcelUtil.getReader("D:\\UserFiles\\桌面\\5.19海大专属页面企业+限制+名称.xlsx", 0);
+        ExcelReader reader = ExcelUtil.getReader("D:\\UserFiles\\桌面\\28家-海大实习生计划参与企业名单 5.25.xlsx", 0);
         List<InternshipExcel> readAll = reader.read().stream().map(s -> {
             InternshipExcel internshipExcel = new InternshipExcel();
             internshipExcel.setSort(String.valueOf(s.get(0)));
-            internshipExcel.setCompanyName(String.valueOf(s.get(1)));
-            internshipExcel.setName(String.valueOf(s.get(2)));
-            internshipExcel.setId(libIntern.getOrDefault(internshipExcel.getName().trim(), new InternshipExcel()).getId());
-            internshipExcel.setDuplicates(libIntern.getOrDefault(internshipExcel.getName().trim(), new InternshipExcel()).getDuplicates());
+            internshipExcel.setCompanyName(String.valueOf(s.get(1)).trim());
+            internshipExcel.setName(String.valueOf(s.get(3)).trim());
+            String key = internshipExcel.getName() + internshipExcel.getCompanyName();
+            internshipExcel.setId(libIntern.getOrDefault(key, new InternshipExcel()).getId());
+            internshipExcel.setDuplicates(libIntern.getOrDefault(key, new InternshipExcel()).getDuplicates());
             return internshipExcel;
         }).collect(Collectors.toList());
 
@@ -94,7 +101,7 @@ class InternshipTest {
     @Test
     void excelText() {
 
-        ExcelReader libReader = ExcelUtil.getReader("D:\\UserFiles\\桌面\\海大id.xlsx", 0);
+        ExcelReader libReader = ExcelUtil.getReader("D:\\UserFiles\\桌面\\current.xlsx", 0);
         List<InternshipExcel> libSchool = libReader.read().stream().map(s -> {
             InternshipExcel schoolExcel = new InternshipExcel();
             schoolExcel.setId(String.valueOf(s.get(4)).trim());
@@ -113,8 +120,32 @@ class InternshipTest {
         }
     }
 
+
+    @Test
+    List<InternshipExcel> dbCompany() throws SQLException {
+        List<InternshipExcel> internshipExcels = Db.use().query("SELECT\n" +
+                "\tipg.id id,\n" +
+                "\tipg.`name` NAME,\n" +
+                "\tcp.`name` companyName \n" +
+                "FROM\n" +
+                "\t`intern_program` ipg\n" +
+                "\tLEFT JOIN company cp ON cp.id = ipg.company_id").stream().map(entity -> {
+            InternshipExcel internshipExcel = new InternshipExcel();
+            internshipExcel.setId(String.valueOf(entity.get("id")));
+            internshipExcel.setName(String.valueOf(entity.get("name")));
+            internshipExcel.setCompanyName(String.valueOf(entity.get("companyName")));
+
+            return internshipExcel;
+        }).collect(Collectors.toList());
+
+        return internshipExcels;
+
+    }
+
+
     @Test
     List<InternshipExcel> db() throws SQLException {
+
         List<InternshipExcel> internshipExcels = Db.use().findAll("intern_program").stream().map(entity -> {
             InternshipExcel internshipExcel = new InternshipExcel();
             internshipExcel.setId(String.valueOf(entity.get("id")));
