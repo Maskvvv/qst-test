800748c11310706ca9232d08231a7f8833462fd8
Maskvvv <1429855087@qq.com>
2024-01-03 11:14:15 +0800

    Userid utils

--- src/test/java/com/zhy/quc/UseridUtils.java
+++ src/test/java/com/zhy/quc/UseridUtils.java
@@ -1,11 +1,14 @@
 package com.zhy.quc;
 
+import cn.hutool.core.io.file.FileAppender;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.zhy.model.Column;
 import com.zhy.model.Table;
 import com.zhy.utils.DbUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
@@ -20,8 +23,9 @@ import java.util.stream.Stream;
  */
 public class UseridUtils {
 
-    private String dbName = "";
+    private String dbName = "athena_competition";
     private List<String> useridColumns = Stream.of(
+            "inviter",
             "customer_id",
             "userid",
             "user_id",
@@ -57,14 +61,16 @@ public class UseridUtils {
     @Test
     public void db() throws SQLException {
 
-        String getTableNameSql = "SELECT\n" +
+        String getTableNameSql =
+                "SELECT\n" +
                 "`TABLE_NAME` \n" +
                 "FROM\n" +
                 "information_schema.TABLES \n" +
                 "WHERE\n" +
                 "table_schema = ?";
 
-        String getColumnNameSql = "SELECT\n" +
+        String getColumnNameSql =
+                "SELECT\n" +
                 "\tCOLUMN_NAME \n" +
                 "FROM\n" +
                 "\tinformation_schema.COLUMNS \n" +
@@ -72,6 +78,13 @@ public class UseridUtils {
                 "\ttable_schema = ? \n" +
                 "\tAND table_name = ?;";
 
+        String updateUseridSql =
+                "UPDATE `{}`.`{}` t1\n" +
+                        "INNER JOIN `quc`.`user` t2 ON t1.`{}` = t2.`customer_id` \n" +
+                        "SET t1.`{}` = t2.`userid`;";
+
+        File file = new File("D:\\UserFiles\\桌面\\userid变更sql\\" + dbName + ".sql");
+        FileAppender appender = new FileAppender(file, 16, true);
 
         List<Table> tableList = DbUtils.query(Table.class, getTableNameSql, dbName);
 
@@ -82,15 +95,29 @@ public class UseridUtils {
                     .collect(Collectors.toSet())), getColumnNameSql, dbName, table.getTableName());
 
 
-            System.out.println("--------------------------" + table.getTableName() + "--------------------");
+            System.out.println("-- ------------------------" + table.getTableName() + "--------------------");
+
+            appender.append("-- ------------------------" + table.getTableName() + "--------------------\n");
 
             for (String useridColumn : useridColumns) {
                 if (columnSet.contains(useridColumn)) {
-                    System.out.println(table.getTableName() + "." + useridColumn);
+                    //System.out.println(table.getTableName() + "." + useridColumn);
+                    appender.append("-- " + useridColumn);
+
+                    String formatSql = StrUtil.format(updateUseridSql, dbName, table.getTableName(), useridColumn, useridColumn);
+                    appender.append(formatSql);
+                    System.out.println(formatSql);
 
                 }
-
             }
+
+            appender.append("");
+            appender.append("-- ------------------------" + table.getTableName() + "--------------------\n");
+            appender.append("");
+            appender.append("");
+
+
+            appender.flush();
         }
 
     }
